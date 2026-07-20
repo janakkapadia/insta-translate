b8cf45ec1a6c5d935434146b3443287810b6e2f4
Janak Kapadia <kapadia.janak@gmail.com>
2026-07-20 20:33:47 +0530

    fix: remove HTML indentation inside whitespace-pre-wrap div for translated strings

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -168,9 +168,7 @@
                             @foreach($locales as $locale)
                             <td class="py-4 px-3 text-sm text-gray-500 align-top group break-words relative">
                                 <div x-show="!editing['{{ $locale }}']">
-                                    <div class="whitespace-pre-wrap pr-6" :class="{'opacity-50 italic': !hasTranslation('{{ $locale }}')}">
-                                        <span x-text="getTranslation('{{ $locale }}') || 'Missing'"></span>
-                                    </div>
+                                    <div class="whitespace-pre-wrap pr-6" :class="{'opacity-50 italic': !hasTranslation('{{ $locale }}')}"><span x-text="getTranslation('{{ $locale }}') || 'Missing'"></span></div>
                                     <button @click="startEdit('{{ $locale }}')" class="absolute top-4 right-2 opacity-0 group-hover:opacity-100 text-indigo-600 hover:text-indigo-900 transition-opacity p-1 bg-white rounded shadow-sm border">
                                         <svg class="h-4 w-4" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15.232 5.232l3.536 3.536m-2.036-5.036a2.5 2.5 0 113.536 3.536L6.5 21.036H3v-3.572L16.732 3.732z" /></svg>
                                     </button>
